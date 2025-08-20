8139e0d98b9332a7a021d3303186a555b6343a47
dhuy <danghoanghuy2018@gmail.com>
2025-08-20 19:14:25 +0700

    Initial project

---
+++ file-storage-system/src/main/java/com/dut/filestorage/file_storage_system/FileStorageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dut.filestorage.file_storage_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStorageSystemApplication.class, args);
+	}
+
+}
